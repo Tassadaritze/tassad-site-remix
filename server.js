be5cd4539d7d96f4046b946600070f398a53d34e
Tassad <Tassadaritze@gmail.com>
2022-06-08 22:19:16 +0300

    Attempt to fix vercel deployment

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const compression = require("compression");
 const morgan = require("morgan");
 const { createRequestHandler } = require("@remix-run/express");
 
-const BUILD_DIR = path.join(process.cwd(), "build");
+// const BUILD_DIR = path.join(process.cwd(), "build");
+const BUILD_DIR = "./build";
 
 const app = express();
 
