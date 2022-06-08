33cab3b9122e68cfd4732c2996270b164f23659c
Tassad <Tassadaritze@gmail.com>
2022-06-08 23:20:19 +0300

    Revert "Attempt to fix vercel deployment"
    This reverts commit af8a2efa2ac857a6e85d2dee92119ad470978a56.

--- server.js
+++ server.js
@@ -29,12 +29,14 @@ app.use(morgan("tiny"));
 
  */
 export default createRequestHandler({ build, mode: process.env.NODE_ENV });
-
+/*
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Express server listening on port ${port}`);
 });
+
+ */
 
 /*
 function purgeRequireCache() {
