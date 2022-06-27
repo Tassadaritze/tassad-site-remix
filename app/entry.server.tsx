5827690f04af0b6cbdece61a7bb0bc0dc7f6c084
Tassad <Tassadaritze@gmail.com>
2022-06-27 11:00:19 +0300

    Initial i18n setup

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,13 +1,43 @@
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
+import { createInstance } from "i18next";
+import Backend from "i18next-fs-backend";
+import { resolve } from "node:path";
+import { initReactI18next } from "react-i18next";
 
-export default function handleRequest(
+import i18next from "./i18next.server";
+import i18n from "./i18n"; // your i18n configuration file
+
+export default async function handleRequest(
     request: Request,
     responseStatusCode: number,
     responseHeaders: Headers,
     remixContext: EntryContext
 ) {
+    // First, we create a new instance of i18next so every request will have a
+    // completely unique instance and not share any state
+    const instance = createInstance();
+
+    // Then we could detect locale from the request
+    const lng = await i18next.getLocale(request);
+    // And here we detect what namespaces the routes about to render want to use
+    const ns = i18next.getRouteNamespaces(remixContext);
+
+    await instance
+        .use(initReactI18next) // Tell our instance to use react-i18next
+        .use(Backend) // Setup our backend
+        .init({
+            ...i18n, // spread the configuration
+            lng, // The locale we detected above
+            ns, // The namespaces the routes about to render wants to use
+            backend: {
+                loadPath: resolve("./public/locales/{{lng}}/{{ns}}.json")
+            }
+        });
+
+    // Then you can render your app wrapped in the I18nextProvider as in the
+    // entry.client file
     const markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
 
     responseHeaders.set("Content-Type", "text/html");
--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,14 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
-import { Link, Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import type { LinksFunction, LoaderFunction, MetaFunction } from "@remix-run/node";
+import { Link, Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
+import { useChangeLanguage } from "remix-i18next";
+import { useTranslation } from "react-i18next";
+
+import i18next from "~/i18next.server";
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
+
+type LoaderData = { locale: string };
 
 export const links: LinksFunction = () => {
     return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
@@ -13,9 +20,33 @@ export const meta: MetaFunction = () => ({
     viewport: "width=device-width,initial-scale=1"
 });
 
+export const loader: LoaderFunction = async ({ request }) => {
+    const locale = await i18next.getLocale(request);
+    return json<LoaderData>({ locale });
+};
+
+export const handle = {
+    // In the handle export, we can add a i18n key with namespaces our route
+    // will need to load. This key can be a single string or an array of strings.
+    // TIP: In most cases, you should set this to your defaultNS from your i18n config
+    // or if you did not set one, set it to the i18next default namespace "translation"
+    i18n: "common"
+};
+
 const App = () => {
+    // Get the locale from the loader
+    const { locale } = useLoaderData<LoaderData>();
+
+    const { i18n } = useTranslation();
+
+    // This hook will change the i18n instance language to the current locale
+    // detected by the loader, this way, when we do something to change the
+    // language, this locale will change and i18next will load the correct
+    // translation files
+    useChangeLanguage(locale);
+
     return (
-        <html lang="en" className="h-full">
+        <html lang={locale} dir={i18n.dir()} className="h-full">
             <head>
                 <Meta />
                 <Links />
