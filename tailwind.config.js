fd7338fc144f1e253a76fb64c62a888379ec6ba5
Tassad <Tassadaritze@gmail.com>
2022-07-01 19:35:07 +0300

    Set up tailwind + radix ui colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,jsx,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./app/**/*.{ts,tsx,jsx,js}"],
+    theme: {
+        extend: {},
+        colors: require("./tailwind-radix-ui-colors")
+    },
+    plugins: [],
+    darkMode: "class"
 };
