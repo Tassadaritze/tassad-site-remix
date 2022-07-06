6412504a546f502ab61e41ece6603b09de0f7b71
Tassad <Tassadaritze@gmail.com>
2022-07-06 15:11:57 +0300

    build: set up commitlint config

---
+++ .commitlintrc.ts
@@ -0,0 +1,9 @@
+export default {
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            ["build", "chore", "ci", "deps", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"]
+        ]
+    }
+};
